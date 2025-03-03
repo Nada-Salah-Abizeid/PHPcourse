9eb22d95e3dc4e31086176ea27833b0953da8420
Nada Salah <nada.salah.it@gmail.com>
2025-03-04 01:31:32 +0200

    Create footer.php

---
+++ Lab5/template/footer.php
@@ -0,0 +1,25 @@
+        <footer id="footer">
+            <div class="container">
+                <div class="footer-nav">
+                    <ul>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="privacy.php">Privacy Policy</a></li>
+                        <li><a href="terms.php">Terms & Conditions</a></li>
+                    </ul>
+                </div>
+
+                <div class="social-media">
+                    <a href="https://facebook.com" target="_blank">Facebook</a>
+                    <a href="https://twitter.com" target="_blank">Twitter</a>
+                    <a href="https://instagram.com" target="_blank">Instagram</a>
+                    <a href="https://linkedin.com" target="_blank">LinkedIn</a>
+                </div>
+
+                <div class="footer-text">
+                    <p>&copy; 2023 Cafeteria Website. All Rights Reserved.</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
